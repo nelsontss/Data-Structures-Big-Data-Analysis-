56f351359c04de4c88ca87c43ae9c845d2c45a5e
mrr37 <36702455+mrr37@users.noreply.github.com>
2018-03-22 18:31:03 +0000

    introdução das funçoes de datas

--- include/mypost.h
+++ include/mypost.h
@@ -2,13 +2,15 @@
 #define __MYPOST_H__
 typedef struct mypost* MyPost;
 
-MyPost create_mypost(char *id, char *title, char *ownerUser);
+MyPost create_mypost(char *id, char *title, char *ownerUser, int data);
 char * get_post_id(MyPost post);
 char * get_post_title(MyPost post);
 char * get_post_ownerUser(MyPost post);
+int get_post_data(MyPost post);
 void set_post_id(MyPost post, char* id);
 void set_post_title(MyPost post, char* title);
 void set_post_ownerUser(MyPost post, char* ownerUser);
+void set_post_data(MyPost post, int data);
 int compare_posts (MyPost p1, MyPost p2);
 void destroy_mypost (MyPost post);
 
