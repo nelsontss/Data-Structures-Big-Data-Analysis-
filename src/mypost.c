830fa9a226278b4fbeb4f2258bf1b8abec538774
mrr37 <36702455+mrr37@users.noreply.github.com>
2018-03-22 17:43:30 +0000

    funçoes da data e inserção na estrutura mypost

--- src/mypost.c
+++ src/mypost.c
@@ -11,11 +11,12 @@ struct mypost
 	int data;
 };
 
-MyPost create_mypost(char* id, char* title, char* ownerUser){
+MyPost create_mypost(char* id, char* title, char* ownerUser, int data){
 	MyPost post= (MyPost) malloc(sizeof(struct mypost));
 	post->id = mystrdup(id);
 	post->title= mystrdup(title);
 	post->ownerUser= mystrdup(ownerUser);
+	post->data= data;
 	return post;
 }
 
@@ -29,6 +30,10 @@ char * get_post_title(MyPost post){
 
 char * get_post_ownerUser(MyPost post){
 	return post ? mystrdup(post->ownerUser) : NULL;
+}
+
+int get_data (MyPost post){
+	return post ? post->data : 0;
 }
 
 void set_post_id(MyPost post, char* id){
@@ -46,6 +51,11 @@ void set_post_ownerUser(MyPost post, char* ownerUser){
 	post->ownerUser=mystrdup(ownerUser);
 }
 
+void set_post_data (MyPost post, int data){
+	free(post->data);
+	post->data = data;
+}
+
 int compare_posts (MyPost p1, MyPost p2){
 	if(p1->data<p2->data)
 		return -1;
@@ -59,5 +69,6 @@ void destroy_mypost (MyPost post){
 	free(post->id);
 	free(post->title);
 	free(post->ownerUser);
+	free (post->data);
 	free(post);
 }
