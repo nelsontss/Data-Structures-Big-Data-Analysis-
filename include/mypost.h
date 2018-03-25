0d9ceaeebe2a0ab3e9fbb6c57aa82209347ef595
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-03-25 12:50:22 +0100

    Adicionada as funçoes get e set do parametro type

--- include/mypost.h
+++ include/mypost.h
@@ -2,15 +2,17 @@
 #define __MYPOST_H__
 typedef struct mypost* MyPost;
 
-MyPost create_mypost(char *id, char *title, char *ownerUser, int data);
+MyPost create_mypost(char *id, char *title, char *ownerUser, int data, int type);
 char * get_post_id(MyPost post);
 char * get_post_title(MyPost post);
 char * get_post_ownerUser(MyPost post);
 int get_post_data(MyPost post);
+int get_post_type (MyPost post);
 void set_post_id(MyPost post, char* id);
 void set_post_title(MyPost post, char* title);
 void set_post_ownerUser(MyPost post, char* ownerUser);
 void set_post_data(MyPost post, int data);
+void set_post_type (MyPost post, int type);
 int compare_posts (MyPost p1, MyPost p2);
 void destroy_mypost (MyPost post);
 
