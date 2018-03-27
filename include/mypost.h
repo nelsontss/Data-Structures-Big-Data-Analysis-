97dc197b76b9e4d3deb058e1438096842eaff35c
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-03-27 16:34:50 +0100

    Add files via upload

--- include/mypost.h
+++ include/mypost.h
@@ -13,6 +13,9 @@ void set_post_title(MyPost post, char* title);
 void set_post_ownerUser(MyPost post, char* ownerUser);
 void set_post_data(MyPost post, int data);
 void set_post_type (MyPost post, int type);
+void set_post_tag (MyPost post, char *tag);
+void insert_tags(MyPost post,char *tags);
+int post_contains_tag(MyPost post,char* tag);
 int compare_posts (MyPost p1, MyPost p2);
 void destroy_mypost (MyPost post);
 
