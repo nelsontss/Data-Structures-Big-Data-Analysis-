4527bb9dfb08a31193e88a43b5b5f64d195d04e6
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-03-28 16:30:07 +0100

    Implementaçao da querry 5

--- include/myuser.h
+++ include/myuser.h
@@ -1,3 +1,4 @@
+#include <glib-2.0/gmodule.h>
 #ifndef __MYUSER_H__
 #define __MYUSER_H__
 typedef struct myuser* MyUser;
@@ -8,11 +9,13 @@ char * get_user_name(MyUser user);
 int get_user_questions(MyUser user);
 int get_user_answers(MyUser user);
 int get_user_totalposts(MyUser user);
+GList * get_user_lastposts (MyUser user);
 void set_user_id(MyUser user, char* name);
 void set_user_name(MyUser user, char* id);
 void set_user_questions(MyUser user, int questions);
 void set_user_answers(MyUser user, int answers);
 void set_user_totalposts(MyUser user, int totalposts);
+void set_lastpost (MyUser user, char * id, int data);
 void aumenta_questions_user(MyUser user);
 void aumenta_answers_user(MyUser user);
 int compare_users (MyUser u1, MyUser u2);
