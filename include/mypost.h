c034e3fe846e4901d3ec6945ea1205c2e6902f00
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-03-29 19:41:59 +0100

    Adiçao do parametro parentID

--- include/mypost.h
+++ include/mypost.h
@@ -7,6 +7,7 @@ MyPost create_mypost(char *id, char *title, char *ownerUser, int data, int type,
 char * get_post_id(MyPost post);
 char * get_post_title(MyPost post);
 char * get_post_ownerUser(MyPost post);
+char * get_post_parentID(MyPost post);
 int get_post_data(MyPost post);
 int get_post_type (MyPost post);
 int get_post_votes (MyPost post);
@@ -15,6 +16,7 @@ GList* post_get_tags(MyPost post);
 void set_post_id(MyPost post, char* id);
 void set_post_title(MyPost post, char* title);
 void set_post_ownerUser(MyPost post, char* ownerUser);
+void set_post_parentID(MyPost post, char* parentID);
 void set_post_data(MyPost post, int data);
 void set_post_type (MyPost post, int type);
 void set_post_tag (MyPost post, char *tag);
