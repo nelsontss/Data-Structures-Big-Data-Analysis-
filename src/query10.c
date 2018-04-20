1ff5996b7a46f3a3310e97c9c71a8758da0e26fe
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-04-20 01:12:45 +0100

    Update query10.c

--- src/query10.c
+++ src/query10.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <loader.h>
 
-long better_answer_aux(GHashTable* users, long id){
+long better_answer_aux(GHashTable* posts, long id){
 	char id_str[10];
 	sprintf(id_str,"%ld",id);
-	MyPost post = get_post(users,id_str);
+	MyPost post = get_post(posts,id_str);
 	return (long)get_best_answer(post);
 }
