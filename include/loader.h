f01d341951e44182226eb90cfb270e8bd3f4356a
nelsontss <36697946+nelsontss@users.noreply.github.com>
2018-04-24 11:14:32 +0100

    Otimizaçao do load

--- include/loader.h
+++ include/loader.h
@@ -7,16 +7,13 @@
 #ifndef LOADER_H
 #define LOADER_H
 
-int load_users(GHashTable* users, char *dump_path);
-int load_posts(GHashTable* users, GHashTable* posts, long* total_questions, long* total_answers, char* dump_path);
-GList* load_userslist(GHashTable* users);
-GList* load_postslist(GHashTable* posts);
-GList* load_questionslist (GList* posts_list);
-GList* load_userslist_rep (GList* users_list);
+int load_users(GHashTable* users, GList** users_list, GList** users_list_rep, char *dump_path);
+int load_posts(GHashTable* users, GHashTable* posts, GList** posts_list, GList** questions_list, long* total_questions, long* total_answers, char* dump_path);
 int load_tags(GHashTable* tags, char* dump_path);
+GList* load_userslist_rep (GList* users_list);
 MyPost get_post (GHashTable* posts, char* id);
 MyUser get_user(GHashTable* users, char* id);
-int date_to_int(Date a);
+
 
 
 #endif
