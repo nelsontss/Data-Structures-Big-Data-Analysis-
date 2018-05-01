fae81e26ced4849fa07e6a565ea0887da8f8eaaa
mrr37 <36702455+mrr37@users.noreply.github.com>
2018-05-01 02:10:46 +0100

    Add files via upload

--- src/myparser.c
+++ src/myparser.c
@@ -4,6 +4,7 @@
 /**
 \ brief Função que abre os ficheiros xml
 @param dump_path Caminho para a pasta que contém os ficheiros xml
+@returns NULL se o doc não existe e o doc se existir
 */
 xmlDocPtr open_doc(char *dump_path){
 	xmlDocPtr doc = xmlParseFile(dump_path);
