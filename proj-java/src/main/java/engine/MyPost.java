a0321f7430450f7e49263e161b690d6983f0f5b0
mrr37 <36702455+mrr37@users.noreply.github.com>
2018-06-10 05:22:02 +0100

    documentação de classes

--- proj-java/src/main/java/engine/MyPost.java
+++ proj-java/src/main/java/engine/MyPost.java
@@ -2,21 +2,21 @@ package engine;
 import java.time.LocalDate;
 import java.util.Iterator;
 /**
- * Write a description of class MyPost here.
+ * Classe que implementa um post
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Grupo 48
+ * 12/6/2018
  */
 public abstract class MyPost
 {
-    // instance variables - replace the example below with your own
+    // Variáveis da classe post
     private String id;
     private String ownerUser;
     private LocalDate data;
     
 
     /**
-     * Constructor for objects of class MyPost
+     * Construtor vazio da classe MyPost
      */
     public MyPost()
     {
@@ -25,14 +25,24 @@ public abstract class MyPost
         this.ownerUser="";
         this.data= LocalDate.now();
     }
-    
+
+    /**
+     * Construtor parametrizado da classe MyPost
+     * @param id ID do post
+     * @param ou ID do proprietário do post
+     * @param data Data do post
+     */
     public MyPost(String id, String ou, LocalDate data)
     {
         this.id =id;
         this.ownerUser=ou;
         this.data= data;
     }
-    
+
+    /**
+     * Construtor de cópia do post
+     * @param mp Post que se pretende copiar
+     */
     public MyPost(MyPost mp)
     {
         this.id =mp.getID();
@@ -42,32 +52,65 @@ public abstract class MyPost
     
     //gets & sets
 
+    /**
+     * Função que retorna o ID do post
+     * @return ID do post
+     */
     public String getID(){
         return this.id;
     }
-    
+
+    /**
+     * Função que retorna o ID do proprietário do post
+     * @return ID do proprietário do post
+     */
     public String getOwnerUser(){
         return this.ownerUser;
     }
-    
+
+    /**
+     * Função que retorna a data do post
+     * @return Data do post
+     */
     public LocalDate getData(){
         return this.data;
     }
-    
+
+    /**
+     * Função que define o ID do post
+     * @param id ID do post dado
+     */
     public void setID(String id){
         this.id=id;
     }
-    
+
+    /**
+     * Função que define o ID do proprietário do post
+     * @param ou ID do proprietário do post dado
+     */
     public void setOwnerUser(String ou){
         this.ownerUser=ou;
     }
-    
+
+    /**
+     * Função que define a data do post
+     * @param data Data do post dado
+     */
     public void setData(LocalDate data){
         this.data=data;
     }
-    
+
+    /**
+     * Função abstrata que devolve a cópia do post
+     * @return Cópia do post
+     */
     public abstract MyPost clone();
-    
+
+    /**
+     * Função que averigua se um post e um objeto são iguais
+     * @param o Objeto a comparar
+     * @return True se são iguais, false se não são iguais
+     */
     public boolean equals(Object o){
         if(this==o) return true;
         if(o==null && this.getClass()!=o.getClass()) return false;
@@ -75,7 +118,11 @@ public abstract class MyPost
         return (this.id.equals(mp.getID()) && this.ownerUser.equals(mp.getOwnerUser())
         && this.data.equals(mp.getData()));
     }
-    
+
+    /**
+     * Função que passa um post para uma String
+     * @return String representativa do post
+     */
     public String toString(){
         StringBuilder sb= new StringBuilder();
         sb.append("ID do post: ");sb.append(this.id);
--- proj-java/src/main/java/engine/Pergunta.java
+++ proj-java/src/main/java/engine/Pergunta.java
@@ -4,14 +4,14 @@ import javax.swing.tree.TreeCellEditor;
 import java.util.*;
 import java.time.LocalDate;
 /**
- * Write a description of class Pergunta here.
+ * Classe que implementa uma pergunta
  *
- * @author (your name)
- * @version (a version number or a date)
+ * Grupo 48
+ * 12/6/2018
  */
 public class Pergunta extends MyPost
 {
-    // instance variables - replace the example below with your own
+    // Variáveis da classe pergunta
     private String title;
     private ArrayList<String> tags;
     private ArrayList<Resposta> resp;
@@ -19,7 +19,7 @@ public class Pergunta extends MyPost
     private int answerCount;
 
     /**
-     * Constructor for objects of class Pergunta
+     * Construtor vazio da classe pergunta
      */
     public Pergunta()
     {
@@ -32,10 +32,20 @@ public class Pergunta extends MyPost
         this.answerCount=0;
     }
 
+    /**
+     * Construtor parametrizado da classe pergunta
+     * @param title Título da pergunta
+     * @param tags Lista de tags da pergunta
+     * @param resp Lista de respostas da pergunta
+     * @param respHash Tabela de hash com as respostas da pergunta
+     * @param ac Numero de respostas à pergunta
+     * @param id ID da pergunta
+     * @param ownerUser ID do proprietário da pergunta
+     * @param data Data da pergunta
+     */
     public Pergunta(String title, ArrayList<String> tags, ArrayList<Resposta> resp, 
     HashMap<String,Resposta> respHash, int ac, String id, String ownerUser,LocalDate data)
     {
-        // initialise instance variables
         super(id, ownerUser, data);
         this.title=title;
         setTags(tags);
@@ -43,7 +53,11 @@ public class Pergunta extends MyPost
         setRespHash(respHash);
         this.answerCount=ac;
     }
-    
+
+    /**
+     * Construtor de cópia da classe pergunta
+     * @param p Pergunta que se pretende copiar
+     */
     public Pergunta(Pergunta p)
     {
         // initialise instance variables
@@ -55,70 +69,121 @@ public class Pergunta extends MyPost
         this.answerCount=p.getAnswerCount();
     }
     //gets & sets
+
+    /**
+     * Função que retorna o titulo da pergunta
+     * @return Titulo da pergunta
+     */
     public String getTitle(){
         return this.title;
     }
-    
+
+    /**
+     * Função que retorna uma lista com as tags da pergunta
+     * @return Lista de tags da pergunta
+     */
     public ArrayList<String> getTags(){
         ArrayList<String> res = new ArrayList<String>();
         for(String s: this.tags)
             res.add(s);
         return res;
     }
-    
+
+    /**
+     * Função que retorna a lista de respostas da pergunta
+     * @return Lista de respostas da pergunta
+     */
     public ArrayList<Resposta> getResp(){
         ArrayList<Resposta> res = new ArrayList<Resposta>();
         for(Resposta r: this.resp)
             res.add(r.clone());
         return res;
     }
-    
+
+    /**
+     * Função que retorna a tabela de hash com as respostas da pergunta
+     * @return Tabela de hash com as respostas da pergunta
+     */
     public HashMap<String,Resposta> getRespHash(){
         HashMap<String,Resposta> res= new HashMap<String,Resposta>();
         for(Map.Entry<String,Resposta> sr: this.respHash.entrySet())
             res.put(sr.getKey(),sr.getValue().clone());
         return res;
     }
-    
+
+    /**
+     * Função que retorna o número de respostas da pergunta
+     * @return Número de respostas da pergunta
+     */
     public int getAnswerCount(){
         return this.answerCount;
     }
-    
+
+    /**
+     * Função que define a lista de tags da pergunta
+     * @param tags Lista de tags dada
+     */
     public void setTags(ArrayList<String> tags){
         this.tags= new ArrayList<String>();
         for(String s: tags){
             this.tags.add(s);
         }
     }
-    
+
+    /**
+     * Função que define a tabela de hash de respostas da pergunta
+     * @param respHash Tabela de hash dada
+     */
     public void setRespHash(HashMap<String,Resposta> respHash){
         this.respHash= new HashMap<String,Resposta>();
         for(Map.Entry<String,Resposta> r: respHash.entrySet())
             this.respHash.put(r.getKey(), r.getValue().clone());
     }
-    
+
+    /**
+     * Função que define a lista de respostas da pergunta
+     * @param resp Lista de respostas dada
+     */
     public void setResp(ArrayList<Resposta> resp){
         this.resp= new ArrayList<Resposta>();
         for(Resposta r: resp)
             this.resp.add(r.clone());
     }
-    
+
+    /**
+     * Função que define o número de respostas da pergunta
+     * @param ac Número de respostas dado
+     */
     public void setAnswerCount(int ac){
         this.answerCount=ac;
     }
-    
+
+    /**
+     * Função que adiciona uma resposta à lista e tabela de hash de respostas
+     * @param r Resposta a adicionar
+     */
     public void addResp(Resposta r){
         this.resp.add(r.clone());
         this.respHash.put(r.getID(),r.clone());
     }
 
-    public Long getBestAnswer(){
+    /**
+     * Função que devolve a melhor resposta feita à pergunta
+     * @return Resposta com melhor pontuação
+     */
+    public String getBestAnswer(){
         TreeSet<Resposta> r = new TreeSet<>(new ComparadorRespostasPorPont());
         for(Resposta x : resp)
             r.add(x);
-        return Long.parseLong(r.first().getID());
+        return r.first().getID();
     }
 
+    /**
+     * Função que averigua se dois utilizadores participaram na resposta à pergunta
+     * @param id1 Utilizador 1
+     * @param id2 Utilizador 2
+     * @return True se ambos participaram, false se pelo menos um não participou
+     */
     public boolean bothParticipated(long id1, long id2){
         int flag = 0;
         if(id1==Long.valueOf(this.getOwnerUser()) && respHash.containsKey(String.valueOf(id2))){
@@ -134,10 +199,19 @@ public class Pergunta extends MyPost
 
     }
 
+    /**
+     * Função que retorna uma cópia da pergunta
+     * @return Cópia da pergunta
+     */
     public Pergunta clone(){
         return new Pergunta(this);
     }
-    
+
+    /**
+     * Função que averigua se uma pergunta e um objeto são iguais
+     * @param o Objeto a comparar
+     * @return True se forem iguais, false se não forem iguais
+     */
     public boolean equals(Object o){
         if(this==o) return true;
         if(o==null && this.getClass()!=o.getClass()) return false;
@@ -145,7 +219,11 @@ public class Pergunta extends MyPost
         return (this.title.equals(p.getTitle()) && this.tags.equals(p.getTags())
         && this.resp.equals(p.getResp()) && this.respHash.equals(p.getRespHash()));
     }
-    
+
+    /**
+     * Função que converte uma pergunta numa String
+     * @return String representativa da pergunta
+     */
     public String toString(){
         StringBuilder sb= new StringBuilder(super.toString());
         sb.append("\nTitulo: ");sb.append(this.title);
